2ba6138685aac5bba3b42e7a5fdd107ba205e002
amanuel <amanueld2730@gmail.com>
2026-02-04 15:00:56 +0300

    update

---
+++ mobile/app.config.ts
@@ -0,0 +1,91 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Explore Adama",
+  slug: "Explore-Adama",
+  version: "1.0.1",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "smarttourism",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.exploreadama",
+  },
+  android: {
+    package: "com.exploreadama",
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/icon.png",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    permissions: [
+      "android.permission.ACCESS_COARSE_LOCATION",
+      "android.permission.ACCESS_FINE_LOCATION",
+      "android.permission.CAMERA",
+      "android.permission.READ_EXTERNAL_STORAGE",
+      "android.permission.WRITE_EXTERNAL_STORAGE",
+      "android.permission.INTERNET",
+      "android.permission.ACCESS_NETWORK_STATE",
+    ],
+    intentFilters: [
+      {
+        action: "VIEW",
+        autoVerify: true,
+        data: [
+          {
+            scheme: "https",
+            host: "exploreadama.com",
+            pathPrefix: "/blog",
+          },
+          {
+            scheme: "smarttourism",
+          },
+        ],
+        category: ["BROWSABLE", "DEFAULT"],
+      },
+    ],
+    config: {
+      googleMaps: {
+        apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: {
+          backgroundColor: "#000000",
+        },
+      },
+    ],
+    "expo-secure-store",
+    "expo-web-browser",
+    "expo-notifications",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "2607ab89-bb5c-4a96-bb7d-6344e2905fe2",
+    },
+    EXPO_PUBLIC_GOOGLE_MAPS_API_KEY:
+      process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
+});
