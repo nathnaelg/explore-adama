7a48a09e856944d456ab815e1dd4f134fb1b4c6e
amanuel <amanueld2730@gmail.com>
2026-02-09 11:21:52 +0300

    fix: Update Vite proxy targets from `localhost` to `127.0.0.1` for improved consistency.fix: Update Vite proxy targets from `localhost` to `127.0.0.1`.

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -24,12 +24,12 @@ export default defineConfig({
     ],
     proxy: {
       "/api": {
-        target: "http://localhost:3005",
+        target: "http://127.0.0.1:3005",
         changeOrigin: true,
         secure: false,
       },
       "/ml": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/ml/, ""),
         secure: false,
